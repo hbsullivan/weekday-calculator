0600e528edea98f1728e3fad6f81b6e343317244
Henry Sullivan <sullivanbhenry@gmail.com>
2022-11-08 16:36:41 -0800

    update name

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: '',
+      title: 'template',
       template: './src/index.html',
       inject: 'body'
     })
