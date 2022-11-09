06a51d85c830c40a5e2eb8ef24a4c2252e6ee736
Henry Sullivan <sullivanbhenry@gmail.com>
2022-11-09 13:18:05 -0800

    BUGFIX: fix bug that caused the wrong day to be displayed
    Co-authored-by: Yodel Guanzon <yodelguanzon@gmail.com>
    Co-authored-by: VW <vera.weikel@gmail.com>
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import WeekDayCalc from './js/calculator';
 
 function handleDateForm(event) {
   event.preventDefault();
-  const date = document.querySelector('#date-input').value;
-  console.log(date);
+  const date = document.querySelector('#date-input').value + ' PST';
   const inputDate =  new WeekDayCalc(Date.parse(date));
-  inputDate.setDate(inputDate.getDate()+1);
-  console.log(inputDate.getDate());
+  inputDate.setDate(inputDate.getDate());
   const dayOfWeek = inputDate.getDayName();
   document.getElementById("day-of-week").innerText = dayOfWeek;
 }
